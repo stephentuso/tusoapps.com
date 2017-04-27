0f4e36ad20fbed500d1cf664b741bbaa122768ea
Stephen Tuso <stephentuso@users.noreply.github.com>
2017-04-27 17:28:02 -0400

    Update gh-pages-push

--- .utility/gh-pages-push.sh
+++ .utility/gh-pages-push.sh
@@ -16,10 +16,10 @@ if [ "$TRAVIS_REPO_SLUG" == "stephentuso/tusoapps.com" ] && [ "$TRAVIS_PULL_REQU
     echo -e "\nCloning gh-pages..."
     git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} gh-pages > /dev/null
 
-    echo -e "/nReplacing files..."
+    echo -e "\nReplacing files..."
     cd gh-pages
     git rm -rf .
-    cp -Rf $HOME/dist/ $HOME/gh-pages/
+    cp -Rf $HOME/dist/ $HOME/gh-pages
     git add -f .
     git commit -m "Update website (travis build #$TRAVIS_BUILD_NUMBER)"
     
