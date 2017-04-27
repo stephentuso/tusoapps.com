dbcac64c69adfb21b902a940b0eefcd0072bf3ac
stephentuso <tusodev@gmail.com>
2017-04-27 04:07:29 -0400

    Update travis config and add gh-pages script

---
+++ .utility/gh-pages-push.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo -e "\nChecking conditions.."
+if [ "$TRAVIS_REPO_SLUG" == "stephentuso/tusoapps.com" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo -e "\nPublishing to gh-pages..."
+
+    cp -R dist $HOME/dist
+
+    cd $HOME
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+
+    echo -e "\nCloning gh-pages..."
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} gh-pages > /dev/null
+
+    echo -e "/nReplacing files..."
+    cd gh-pages
+    git rm -rf .
+    cp -Rf $HOME/dist .
+    git add -f .
+    git commit -m "Update website (travis build #$TRAVIS_BUILD_NUMBER)"
+
+    echo -e "Publishing..."
+    git push -fq origin gh-pages > /dev/null
+
+    echo -e "Pushed updated files to gh-pages"
+
+else
+    echo -e "Not publishing to gh-pages"
+fi
